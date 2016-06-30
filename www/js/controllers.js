70367f9d9ea55cc5fd21b75beb2a54a587fe5311
omkar.kulkarni@zconsolutions.com <omkar.kulkarni@zconsolutions.com>
2016-06-30 14:04:40 +0530

    change for append json

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,95 +1,88 @@
 
 
 
-
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope,$http) {
-	 //$scope.people = [];
+	 
 	  
     $scope.loadPeople = function() {
-		//$scope.people={};
-		    var jsonPeople=[ {"firstName":"ABC", "lastName":"ABC"},
-    {"firstName":"EFG", "lastName":"EFG"},
-    {"firstName":"HIJ", "lastName":"HIJ"},
-	 {"firstName":"LMN", "lastName":"LMN"}];
-       
-           $scope.people = jsonPeople;
-	 
-	 
-    };
+	    var jsonPeople=[ {"firstName":"ABC", "lastName":"ABC"},
+		{"firstName":"EFG", "lastName":"EFG"},
+		{"firstName":"HIJ", "lastName":"HIJ"},
+		{"firstName":"LMN", "lastName":"LMN"}];
+        $scope.people = jsonPeople;
+	};
 	$scope.loadPeople2 = function() {
-		//$scope.people ={};
-		    var jsonPeoplee=[ {"firstName":"OPQ", "lastName":"OPQ"},
+		   var jsonPeople=[ {"firstName":"OPQ", "lastName":"OPQ"},
     {"firstName":"RST", "lastName":"RST"},
     {"firstName":"UVW", "lastName":"UVW"},
 	{"firstName":"XYZ", "lastName":"XYZ"},
     {"firstName":"ABC", "lastName":"ABC"},
 	{"firstName":"LMN", "lastName":"LMN"}];
-       
-           $scope.people = jsonPeoplee;
-	 
-	 
-     
-
-    };
-
+          $scope.people = jsonPeople;
+	  };	
 })
 
-.controller('ChatsCtrl',function($scope,$http){
-  
-  $scope.loadPeople = function(event) {
-	 
-		 $scope.outputs = [];
-			 $scope.inputs = [];
-		 if(event.target.id==1)
-		 {
-		var object=  $http.get('js/link1.json').success(function(response){
-			$scope.inputs = response;
-			 });
-		 }
-	 else{
-	var object=$http.get('js/text.json').success(function(response){
-		$scope.inputs = response;
-	 	});
-	 }
-    };
+.controller('ChatsCtrl',function($scope,$http,$q){
+				$scope.inputs=[];
+				$scope.loadPeople = function(event) {
+				$scope.outputs = {};
+					
+				var object1=  $http.get('js/link1.json').success(function(response){
+						$scope.inputs=response;
+				});
+				
+				};
+				$scope.loadPeople2 = function(event) {
+						var object2=$http.get('js/text.json').success(function(response){
+						$scope.inputs = [].concat($scope.inputs , response)
+				});
+		}
 	
 })
+
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
-.controller('AccountCtrl', function($scope) {
- $scope.people = [];
-	 
-    $scope.loadPeople = function() {
-		  $http.get('js/data.json').success(function(data){
-			  console.log(data);
-			   $scope.stdFormData = data.form.data;
-			   console.log(data.form.data);
-                $scope.stdFormTemplate = data.form.template;
- console.log(data.form.template);
-                var element = angular.element(document.getElementById("my-form"));
-                element.html('<dynamic-form class="col-md-12" template="stdFormTemplate" ng-model="stdFormData"></dynamic-form>');
-				
-                $compile(element.contents())($scope);  
-		  });
 
-    };
-	$scope.loadPeople2 = function() {
-		$scope.people ={};
-		   
-        $http.get('js/data1.json').success(function(data){
-			  console.log(data);
-			   $scope.stdFormData = data.form.data;
-			   console.log(data.form.data);
-                $scope.stdFormTemplate = data.form.template;
- console.log(data.form.template);
-                var element = angular.element(document.getElementById("my-form"));
-                element.html('<dynamic-form class="col-md-12" template="stdFormTemplate" ng-model="stdFormData"></dynamic-form>');
-				
-                $compile(element.contents())($scope);  
-		  });
-	  };
+
+.controller('AccountCtrl', function($scope,$http,$compile) {
+
+    $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}, {name:'no-clone', allowClone:false}];
+        $scope.droppedObjects1 = [];
+        $scope.droppedObjects2= [];
+        $scope.onDropComplete1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index == -1)
+            $scope.droppedObjects1.push(data);
+        }
+        $scope.onDragSuccess1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index > -1) {
+                $scope.droppedObjects1.splice(index, 1);
+            }
+        }
+        $scope.onDragSuccess2=function(data,evt){
+            var index = $scope.droppedObjects2.indexOf(data);
+            if (index > -1) {
+                $scope.droppedObjects2.splice(index, 1);
+            }
+        }
+        $scope.onDropComplete2=function(data,evt){
+            var index = $scope.droppedObjects2.indexOf(data);
+            if (index == -1) {
+                $scope.droppedObjects2.push(data);
+            }
+        }
+        var inArray = function(array, obj) {
+            var index = array.indexOf(obj);
+        }
+
+	
+	
 });
+
+
+
